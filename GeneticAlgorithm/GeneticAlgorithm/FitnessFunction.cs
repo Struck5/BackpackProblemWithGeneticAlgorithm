975865bba58a11bbde6b8b0103122b0873db4129
Struck5 <janphilippstruck@gmx.de>
2015-07-08 12:34:19 +0200

    edited Fitness-Function

--- GeneticAlgorithm/GeneticAlgorithm/FitnessFunction.cs
+++ GeneticAlgorithm/GeneticAlgorithm/FitnessFunction.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,7 +9,7 @@ namespace GeneticAlgorithm
 {
     class FitnessFunction
     {
-        public static int MaximalGewicht = 100;
+        public static int MaxValue = 250;
 
         //Bewertungskriterien:
         //Eventuell nicht nur Gold sondern auch Gold pro Gewicht Verhältnis beurteilen?
@@ -17,13 +18,23 @@ namespace GeneticAlgorithm
         {
             // Mögliche FitnessFunktionen, nicht final, nicht alle zusammen verwenden!!!
 
-            genom.Fitness += genom.ImSack.Sum(t => t.Worth)/100f; //Mehr Gold => höhere Fitness!
+            if (genom.ImSack.Sum(t => t.Weight) >= MaxValue*1.2f)
+            {
+                genom.Fitness = 0;
+                return;
+            }
 
-            genom.Fitness *= (genom.ImSack.Sum(t => t.Worth) / 50f); //Gold = 0 => Fitness = 0; Je näher an Gold = 0 desto kleiner Fitness!
+            genom.Fitness += genom.ImSack.Sum(t => t.Worth); //Mehr Gold => höhere Fitness!
+            Console.WriteLine(genom.ImSack.Sum(t => t.Worth));
+            Console.WriteLine(genom.Fitness);
 
-            genom.Fitness -= genom.ImSack.Sum(t => t.Weight) - MaximalGewicht; //Maximalgewicht überschritten => Fitness sehr klein/direkt auf 0
-
-            genom.Fitness *= Math.Abs(10 / (genom.ImSack.Sum(t => t.Weight) - MaximalGewicht)); //Maximalgewicht perfekt/sehr nahe getroffen = Bonuspunkte! Je weiter vom MaximalGewicht entfernt, desto kleiner die Fitness!
+            genom.Fitness += (-((genom.ImSack.Sum(t => t.Weight) - MaxValue) * (genom.ImSack.Sum(t => t.Weight) - MaxValue))); //Maximalgewicht überschritten => Fitness sehr klein/direkt auf 0
+            Console.WriteLine((genom.ImSack.Sum(t => t.Weight) - MaxValue));
+            Console.WriteLine((-((genom.ImSack.Sum(t => t.Weight) - MaxValue) * (genom.ImSack.Sum(t => t.Weight) - MaxValue))));
+            Console.WriteLine(genom.Fitness);
+            Console.ReadKey();
+            /*genom.Fitness *= (genom.ImSack.Sum(t => t.Weight) - MaxValue); //Maximalgewicht perfekt/sehr nahe getroffen = Bonuspunkte! Je weiter vom MaximalGewicht entfernt, desto kleiner die Fitness!
+            genom.Fitness *= (genom.ImSack.Sum(t => t.Worth) / 50f); //Gold = 0 => Fitness = 0; Je näher an Gold = 0 desto kleiner Fitness!*/
         }
     }
 }
